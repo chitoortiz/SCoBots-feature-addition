95c70321b9467cced1f345f6df7afe057a07e35c
hongngoc-doan <hn.doantran@gmail.com>
2025-01-28 18:30:39 +0100

    tree rules

---
+++ tree_rules.py
@@ -0,0 +1,377 @@
+def predict(input_features):
+    if input_features[118] <= 0.27:
+        if input_features[119] <= 0.46:
+            if input_features[120] <= -0.17:
+                if input_features[103] <= -0.49:
+                    if input_features[119] <= -0.47:
+                        if input_features[118] <= -1.38:
+                            if input_features[121] <= 1.62:
+                                return '6'
+                            else:
+                                return '8'
+                        else:
+                            if input_features[46] <= 2.67:
+                                return '6'
+                            else:
+                                return '1'
+                    else:
+                        if input_features[120] <= -1.08:
+                            if input_features[77] <= 1.09:
+                                return '8'
+                            else:
+                                return '8'
+                        else:
+                            if input_features[107] <= 1.63:
+                                return '8'
+                            else:
+                                return '2'
+                else:
+                    if input_features[118] <= -0.91:
+                        if input_features[82] <= 3.77:
+                            if input_features[83] <= 3.35:
+                                return '6'
+                            else:
+                                return '6'
+                        else:
+                            if input_features[102] <= 0.35:
+                                return '6'
+                            else:
+                                return '8'
+                    else:
+                        if input_features[41] <= -0.23:
+                            if input_features[22] <= 0.32:
+                                return '6'
+                            else:
+                                return '6'
+                        else:
+                            if input_features[102] <= 3.14:
+                                return '6'
+                            else:
+                                return '4'
+            else:
+                if input_features[41] <= 0.13:
+                    if input_features[1] <= -0.63:
+                        if input_features[36] <= -0.10:
+                            if input_features[135] <= 0.68:
+                                return '8'
+                            else:
+                                return '8'
+                        else:
+                            if input_features[51] <= 0.53:
+                                return '8'
+                            else:
+                                return '6'
+                    else:
+                        if input_features[119] <= -0.50:
+                            if input_features[118] <= -0.01:
+                                return '6'
+                            else:
+                                return '1'
+                        else:
+                            if input_features[36] <= 0.32:
+                                return '6'
+                            else:
+                                return '6'
+                else:
+                    if input_features[2] <= -0.15:
+                        if input_features[106] <= -0.49:
+                            if input_features[109] <= 2.15:
+                                return '2'
+                            else:
+                                return '2'
+                        else:
+                            if input_features[118] <= -1.86:
+                                return '6'
+                            else:
+                                return '5'
+                    else:
+                        if input_features[119] <= -0.50:
+                            if input_features[118] <= -1.24:
+                                return '6'
+                            else:
+                                return '5'
+                        else:
+                            if input_features[36] <= 0.24:
+                                return '1'
+                            else:
+                                return '6'
+        else:
+            if input_features[118] <= -0.91:
+                if input_features[0] <= 0.24:
+                    if input_features[74] <= 0.33:
+                        if input_features[105] <= 1.05:
+                            if input_features[108] <= 1.02:
+                                return '8'
+                            else:
+                                return '8'
+                        else:
+                            if input_features[118] <= -1.29:
+                                return '8'
+                            else:
+                                return '4'
+                    else:
+                        if input_features[106] <= -1.58:
+                            if input_features[52] <= 2.45:
+                                return '2'
+                            else:
+                                return '3'
+                        else:
+                            if input_features[106] <= 2.04:
+                                return '8'
+                            else:
+                                return '7'
+                else:
+                    if input_features[118] <= -1.81:
+                        if input_features[104] <= 2.08:
+                            if input_features[136] <= 5.23:
+                                return '8'
+                            else:
+                                return '3'
+                        else:
+                            if input_features[20] <= 3.14:
+                                return '8'
+                            else:
+                                return '4'
+                    else:
+                        if input_features[140] <= 0.44:
+                            if input_features[104] <= 2.02:
+                                return '8'
+                            else:
+                                return '4'
+                        else:
+                            if input_features[4] <= 1.08:
+                                return '8'
+                            else:
+                                return '8'
+            else:
+                if input_features[152] <= -0.14:
+                    if input_features[118] <= -0.25:
+                        if input_features[108] <= -0.39:
+                            if input_features[73] <= 0.92:
+                                return '2'
+                            else:
+                                return '4'
+                        else:
+                            if input_features[120] <= -0.32:
+                                return '4'
+                            else:
+                                return '4'
+                    else:
+                        if input_features[120] <= -0.13:
+                            if input_features[84] <= 3.20:
+                                return '7'
+                            else:
+                                return '4'
+                        else:
+                            if input_features[22] <= 3.55:
+                                return '7'
+                            else:
+                                return '4'
+                else:
+                    if input_features[36] <= 0.74:
+                        if input_features[107] <= 0.76:
+                            if input_features[76] <= 1.69:
+                                return '8'
+                            else:
+                                return '7'
+                        else:
+                            if input_features[119] <= 1.39:
+                                return '2'
+                            else:
+                                return '4'
+                    else:
+                        if input_features[118] <= -0.11:
+                            if input_features[119] <= 5.12:
+                                return '8'
+                            else:
+                                return '4'
+                        else:
+                            if input_features[120] <= 0.16:
+                                return '8'
+                            else:
+                                return '7'
+    else:
+        if input_features[119] <= -0.47:
+            if input_features[84] <= 2.69:
+                if input_features[118] <= 1.59:
+                    if input_features[111] <= -4.57:
+                        if input_features[119] <= -2.34:
+                            if input_features[4] <= 0.17:
+                                return '1'
+                            else:
+                                return '5'
+                        else:
+                            if input_features[110] <= 0.78:
+                                return '2'
+                            else:
+                                return '5'
+                    else:
+                        if input_features[158] <= 1.13:
+                            if input_features[122] <= -1.76:
+                                return '5'
+                            else:
+                                return '5'
+                        else:
+                            if input_features[71] <= 1.42:
+                                return '5'
+                            else:
+                                return '7'
+                else:
+                    if input_features[77] <= 2.56:
+                        if input_features[104] <= -0.79:
+                            if input_features[105] <= -0.56:
+                                return '5'
+                            else:
+                                return '5'
+                        else:
+                            if input_features[127] <= 5.84:
+                                return '5'
+                            else:
+                                return '7'
+                    else:
+                        if input_features[103] <= -2.36:
+                            return '7'
+                        else:
+                            if input_features[120] <= 3.84:
+                                return '5'
+                            else:
+                                return '7'
+            else:
+                if input_features[121] <= 0.29:
+                    if input_features[81] <= 3.11:
+                        if input_features[80] <= -0.01:
+                            if input_features[119] <= -1.40:
+                                return '5'
+                            else:
+                                return '7'
+                        else:
+                            if input_features[129] <= 7.30:
+                                return '5'
+                            else:
+                                return '7'
+                    else:
+                        if input_features[118] <= 3.13:
+                            if input_features[82] <= 3.05:
+                                return '7'
+                            else:
+                                return '5'
+                        else:
+                            return '7'
+                else:
+                    if input_features[102] <= -1.34:
+                        if input_features[120] <= 1.05:
+                            if input_features[38] <= 0.23:
+                                return '5'
+                            else:
+                                return '6'
+                        else:
+                            if input_features[122] <= 1.48:
+                                return '7'
+                            else:
+                                return '5'
+                    else:
+                        if input_features[29] <= 1.46:
+                            if input_features[36] <= 2.51:
+                                return '7'
+                            else:
+                                return '5'
+                        else:
+                            if input_features[124] <= 0.61:
+                                return '6'
+                            else:
+                                return '5'
+        else:
+            if input_features[39] <= 0.13:
+                if input_features[118] <= 0.74:
+                    if input_features[124] <= 0.27:
+                        if input_features[119] <= 0.46:
+                            if input_features[1] <= 2.12:
+                                return '5'
+                            else:
+                                return '7'
+                        else:
+                            if input_features[152] <= 0.64:
+                                return '7'
+                            else:
+                                return '4'
+                    else:
+                        if input_features[94] <= 0.83:
+                            if input_features[88] <= 5.62:
+                                return '7'
+                            else:
+                                return '4'
+                        else:
+                            if input_features[79] <= 2.69:
+                                return '5'
+                            else:
+                                return '7'
+                else:
+                    if input_features[124] <= 0.77:
+                        if input_features[119] <= 0.46:
+                            if input_features[152] <= 0.51:
+                                return '7'
+                            else:
+                                return '7'
+                        else:
+                            if input_features[105] <= 0.51:
+                                return '7'
+                            else:
+                                return '7'
+                    else:
+                        if input_features[98] <= 0.87:
+                            if input_features[73] <= 0.10:
+                                return '5'
+                            else:
+                                return '7'
+                        else:
+                            if input_features[94] <= 1.01:
+                                return '6'
+                            else:
+                                return '5'
+            else:
+                if input_features[118] <= 2.30:
+                    if input_features[120] <= -0.03:
+                        if input_features[119] <= 1.39:
+                            if input_features[103] <= 0.13:
+                                return '7'
+                            else:
+                                return '5'
+                        else:
+                            if input_features[152] <= -0.78:
+                                return '7'
+                            else:
+                                return '4'
+                    else:
+                        if input_features[119] <= 0.46:
+                            if input_features[103] <= -0.18:
+                                return '7'
+                            else:
+                                return '5'
+                        else:
+                            if input_features[34] <= 1.59:
+                                return '7'
+                            else:
+                                return '7'
+                else:
+                    if input_features[71] <= 0.59:
+                        if input_features[102] <= -0.53:
+                            if input_features[104] <= -0.44:
+                                return '2'
+                            else:
+                                return '5'
+                        else:
+                            if input_features[121] <= 1.62:
+                                return '7'
+                            else:
+                                return '7'
+                    else:
+                        if input_features[38] <= 1.25:
+                            if input_features[119] <= 0.46:
+                                return '7'
+                            else:
+                                return '7'
+                        else:
+                            if input_features[118] <= 3.62:
+                                return '7'
+                            else:
+                                return '7'
